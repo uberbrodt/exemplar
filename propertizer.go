5e6e54666171f96665e024f49e603ca8ea61437f
Chris Brodt <chris@uberbrodt.net>
2015-12-08 16:20:24 -0600

    Finished basic functionality and a test

--- propertizer.go
+++ propertizer.go
@@ -93,7 +93,7 @@ func main() {
 	g.Printf("\n")
 	g.Printf("package %s", g.pkg.name)
 	g.Printf("\n")
-	g.Printf("import \"fmt\"\n") // Used by all methods.
+	///g.Printf("import \"fmt\"\n") // Used by all methods.
 
 	// Run generate for each type.
 	for _, typeName := range types {
@@ -142,6 +142,12 @@ type File struct {
 	// These fields are reset for each type being generated.
 	typeName string  // Name of the struct type.
 	fields   []Field // Accumulator for the structs fields
+	imports  []Import
+}
+
+type Import struct {
+	ImportedName string
+	Path         *ast.BasicLit
 }
 
 type Package struct {
@@ -221,20 +227,31 @@ func (g *Generator) parsePackage(directory string, names []string, text interfac
 // check type-checks the package. The package must be OK to proceed.
 func (pkg *Package) check(fs *token.FileSet, astFiles []*ast.File) {
 	pkg.defs = make(map[*ast.Ident]types.Object)
-	config := types.Config{FakeImportC: true}
+	config := types.Config{FakeImportC: true, Error: func(err error) {
+		cErr := err.(types.Error)
+		if cErr.Soft {
+			return
+		}
+		if strings.Contains(cErr.Msg, "has no field or method") {
+			log.Printf("IGNORED: during package check: %s", cErr.Msg)
+			return
+		}
+		log.Fatalf("checking package: %s", cErr.Msg)
+	}}
 	info := &types.Info{
 		Defs: pkg.defs,
 	}
-	typesPkg, err := config.Check(pkg.dir, fs, astFiles, info)
-	if err != nil {
-		log.Fatalf("checking package: %s", err)
-	}
+	typesPkg, _ := config.Check(pkg.dir, fs, astFiles, info)
+	//if err != nil {
+	//log.Fatalf("checking package: %s", err)
+	//	}
 	pkg.typesPkg = typesPkg
 }
 
 // generate produces the String method for the named type.
 func (g *Generator) generate(typeName string) {
 	fields := make([]Field, 0, 100)
+	imports := make([]Import, 0, 100)
 	for _, file := range g.pkg.files {
 		// Set the state for this run of the walker.
 		file.typeName = typeName
@@ -242,38 +259,24 @@ func (g *Generator) generate(typeName string) {
 		if file.file != nil {
 			ast.Inspect(file.file, file.genDecl)
 			fields = append(fields, file.fields...)
+			imports = append(imports, file.imports...)
+
 		}
 	}
 
+	//g.Printf("import \"fmt\"\n") // Used by all methods.
 	if len(fields) == 0 {
 		log.Fatalf("no fields defined for type %s", typeName)
 	}
-	//runs := splitIntoRuns(values)
-	// The decision of which pattern to use depends on the number of
-	// runs in the numbers. If there's only one, it's easy. For more than
-	// one, there's a tradeoff between complexity and size of the data
-	// and code vs. the simplicity of a map. A map takes more space,
-	// but so does the code. The decision here (crossover at 10) is
-	// arbitrary, but considers that for large numbers of runs the cost
-	// of the linear scan in the switch might become important, and
-	// rather than use yet another algorithm such as binary search,
-	// we punt and use a map. In any case, the likelihood of a map
-	// being necessary for any realistic example other than bitmasks
-	// is very low. And bitmasks probably deserve their own analysis,
-	// to be done some other day.
+
+	for _, imprt := range imports {
+		g.Printf("import %s \n", imprt.Path.Value)
+	}
 
 	for _, field := range fields {
 		g.buildAccessor(field, typeName)
 		g.buildMutator(field, typeName)
 	}
-	//switch {
-	//case len(runs) == 1:
-	//	g.buildOneRun(runs, typeName)
-	//case len(runs) <= 10:
-	//	g.buildMultipleRuns(runs, typeName)
-	//default:
-	//	g.buildMap(runs, typeName)
-	//}
 }
 func (g *Generator) buildAccessor(f Field, typeName string) {
 	g.Printf("func (this *%s) Get%s() %s {\n", typeName, f.name,
@@ -285,38 +288,6 @@ func (g *Generator) buildAccessor(f Field, typeName string) {
 func (g *Generator) buildMutator(f Field, typeName string) {
 	//TODO: Implement
 }
-
-// splitIntoRuns breaks the values into runs of contiguous sequences.
-// For example, given 1,2,3,5,6,7 it returns {1,2,3},{5,6,7}.
-// The input slice is known to be non-empty.
-//func splitIntoRuns(values []Value) [][]Value {
-//	// We use stable sort so the lexically first name is chosen for equal elements.
-//	sort.Stable(byValue(values))
-//	// Remove duplicates. Stable sort has put the one we want to print first,
-//	// so use that one. The String method won't care about which named constant
-//	// was the argument, so the first name for the given value is the only one to keep.
-//	// We need to do this because identical values would cause the switch or map
-//	// to fail to compile.
-//	j := 1
-//	for i := 1; i < len(values); i++ {
-//		if values[i].value != values[i-1].value {
-//			values[j] = values[i]
-//			j++
-//		}
-//	}
-//	values = values[:j]
-//	runs := make([][]Value, 0, 10)
-//	for len(values) > 0 {
-//		// One contiguous sequence per outer loop.
-//		i := 1
-//		for i < len(values) && values[i].value == values[i-1].value+1 {
-//			i++
-//		}
-//		runs = append(runs, values[:i])
-//		values = values[i:]
-//	}
-//	return runs
-//}
 
 // format returns the gofmt-ed contents of the Generator's buffer.
 func (g *Generator) format() []byte {
@@ -336,20 +307,6 @@ type Field struct {
 	name     string // The name of the field.
 	typeName string //string representation of the Go Type of the field
 }
-
-// byValue lets us sort the constants into increasing order.
-// We take care in the Less method to sort in signed or unsigned order,
-// as appropriate.
-//type byValue []Value
-//
-//func (b byValue) Len() int      { return len(b) }
-//func (b byValue) Swap(i, j int) { b[i], b[j] = b[j], b[i] }
-//func (b byValue) Less(i, j int) bool {
-//	if b[i].signed {
-//		return int64(b[i].value) < int64(b[j].value)
-//	}
-//	return b[i].value < b[j].value
-//}
 
 // genDecl processes one declaration clause.
 func (f *File) genDecl(node ast.Node) bool {
@@ -402,21 +359,18 @@ func (f *File) genDecl(node ast.Node) bool {
 				}
 				fieldObj, _, _ := types.LookupFieldOrMethod(typesObj.Type(), false, f.pkg.typesPkg, field.Name)
 
-				//Get the underlying type string
-				typeStr := fieldObj.Type().Underlying().String()
+				typeStr := fieldObj.Type().String()
 
-				//if info&types.IsInteger == 0 {
-				//	log.Fatalf("can't handle non-integer constant type %s", typ)
-				//}
-				//value := obj.(*types.Const).Val() // Guaranteed to succeed as this is CONST.
-				//if value.Kind() != exact.Int {
-				//	log.Fatalf("can't happen: constant is not an integer %s", name)
-				//}
-				//i64, isInt := exact.Int64Val(value)
-				//u64, isUint := exact.Uint64Val(value)
-				//if !isInt && !isUint {
-				//	log.Fatalf("internal error: value of %s is not an integer: %s", name, value.String())
-				//}
+				if strings.Contains(typeStr, ".") {
+					typeSplit := strings.Split(typeStr, ".")
+					for _, importSpec := range f.file.Imports {
+						if matchImport(typeSplit[0], importSpec) {
+							if !importExists(typeSplit[0], f.imports) {
+								f.imports = append(f.imports, Import{typeSplit[0], importSpec.Path})
+							}
+						}
+					}
+				}
 
 				v := Field{
 					name:     field.Name,
@@ -429,177 +383,15 @@ func (f *File) genDecl(node ast.Node) bool {
 	return false
 }
 
-// Helpers
-
-// usize returns the number of bits of the smallest unsigned integer
-// type that will hold n. Used to create the smallest possible slice of
-// integers to use as indexes into the concatenated strings.
-func usize(n int) int {
-	switch {
-	case n < 1<<8:
-		return 8
-	case n < 1<<16:
-		return 16
-	default:
-		// 2^32 is enough constants for anyone.
-		return 32
-	}
+func matchImport(objectImportId string, importSpec *ast.ImportSpec) bool {
+	//TODO: Handle nested packages
+	return objectImportId == strings.Replace(importSpec.Path.Value, "\"", "", -1)
 }
-
-// declareIndexAndNameVars declares the index slices and concatenated names
-// strings representing the runs of values.
-//func (g *Generator) declareIndexAndNameVars(runs [][]Value, typeName string) {
-//	var indexes, names []string
-//	for i, run := range runs {
-//		index, name := g.createIndexAndNameDecl(run, typeName, fmt.Sprintf("_%d", i))
-//		indexes = append(indexes, index)
-//		names = append(names, name)
-//	}
-//	g.Printf("const (\n")
-//	for _, name := range names {
-//		g.Printf("\t%s\n", name)
-//	}
-//	g.Printf(")\n\n")
-//	g.Printf("var (")
-//	for _, index := range indexes {
-//		g.Printf("\t%s\n", index)
-//	}
-//	g.Printf(")\n\n")
-//}
-
-// declareIndexAndNameVar is the single-run version of declareIndexAndNameVars
-//func (g *Generator) declareIndexAndNameVar(run []Value, typeName string) {
-//	index, name := g.createIndexAndNameDecl(run, typeName, "")
-//	g.Printf("const %s\n", name)
-//	g.Printf("var %s\n", index)
-//}
-//
-// createIndexAndNameDecl returns the pair of declarations for the run. The caller will add "const" and "var".
-//func (g *Generator) createIndexAndNameDecl(run []Value, typeName string, suffix string) (string, string) {
-//	b := new(bytes.Buffer)
-//	indexes := make([]int, len(run))
-//	for i := range run {
-//		b.WriteString(run[i].name)
-//		indexes[i] = b.Len()
-//	}
-//	nameConst := fmt.Sprintf("_%s_name%s = %q", typeName, suffix, b.String())
-//	nameLen := b.Len()
-//	b.Reset()
-//	fmt.Fprintf(b, "_%s_index%s = [...]uint%d{0, ", typeName, suffix, usize(nameLen))
-//	for i, v := range indexes {
-//		if i > 0 {
-//			fmt.Fprintf(b, ", ")
-//		}
-//		fmt.Fprintf(b, "%d", v)
-//	}
-//	fmt.Fprintf(b, "}")
-//	return b.String(), nameConst
-//}
-
-// declareNameVars declares the concatenated names string representing all the values in the runs.
-//func (g *Generator) declareNameVars(runs [][]Value, typeName string, suffix string) {
-//	g.Printf("const _%s_name%s = \"", typeName, suffix)
-//	for _, run := range runs {
-//		for i := range run {
-//			g.Printf("%s", run[i].name)
-//		}
-//	}
-//	g.Printf("\"\n")
-//}
-//
-//// buildOneRun generates the variables and String method for a single run of contiguous values.
-//func (g *Generator) buildOneRun(runs [][]Value, typeName string) {
-//	values := runs[0]
-//	g.Printf("\n")
-//	g.declareIndexAndNameVar(values, typeName)
-//	// The generated code is simple enough to write as a Printf format.
-//	lessThanZero := ""
-//	if values[0].signed {
-//		lessThanZero = "i < 0 || "
-//	}
-//	if values[0].value == 0 { // Signed or unsigned, 0 is still 0.
-//		g.Printf(stringOneRun, typeName, usize(len(values)), lessThanZero)
-//	} else {
-//		g.Printf(stringOneRunWithOffset, typeName, values[0].String(), usize(len(values)), lessThanZero)
-//	}
-//}
-
-// Arguments to format are:
-//	[1]: type name
-//	[2]: size of index element (8 for uint8 etc.)
-//	[3]: less than zero check (for signed types)
-const stringOneRun = `func (i %[1]s) String() string {
-	if %[3]si >= %[1]s(len(_%[1]s_index)-1) {
-		return fmt.Sprintf("%[1]s(%%d)", i)
+func importExists(pathName string, imports []Import) bool {
+	for _, val := range imports {
+		if pathName == val.ImportedName {
+			return true
+		}
 	}
-	return _%[1]s_name[_%[1]s_index[i]:_%[1]s_index[i+1]]
+	return false
 }
-`
-
-// Arguments to format are:
-//	[1]: type name
-//	[2]: lowest defined value for type, as a string
-//	[3]: size of index element (8 for uint8 etc.)
-//	[4]: less than zero check (for signed types)
-/*
- */
-const stringOneRunWithOffset = `func (i %[1]s) String() string {
-	i -= %[2]s
-	if %[4]si >= %[1]s(len(_%[1]s_index)-1) {
-		return fmt.Sprintf("%[1]s(%%d)", i + %[2]s)
-	}
-	return _%[1]s_name[_%[1]s_index[i] : _%[1]s_index[i+1]]
-}
-`
-
-// buildMultipleRuns generates the variables and String method for multiple runs of contiguous values.
-// For this pattern, a single Printf format won't do.
-//func (g *Generator) buildMultipleRuns(runs [][]Value, typeName string) {
-//	g.Printf("\n")
-//	g.declareIndexAndNameVars(runs, typeName)
-//	g.Printf("func (i %s) String() string {\n", typeName)
-//	g.Printf("\tswitch {\n")
-//	for i, values := range runs {
-//		if len(values) == 1 {
-//			g.Printf("\tcase i == %s:\n", &values[0])
-//			g.Printf("\t\treturn _%s_name_%d\n", typeName, i)
-//			continue
-//		}
-//		g.Printf("\tcase %s <= i && i <= %s:\n", &values[0], &values[len(values)-1])
-//		if values[0].value != 0 {
-//			g.Printf("\t\ti -= %s\n", &values[0])
-//		}
-//		g.Printf("\t\treturn _%s_name_%d[_%s_index_%d[i]:_%s_index_%d[i+1]]\n",
-//			typeName, i, typeName, i, typeName, i)
-//	}
-//	g.Printf("\tdefault:\n")
-//	g.Printf("\t\treturn fmt.Sprintf(\"%s(%%d)\", i)\n", typeName)
-//	g.Printf("\t}\n")
-//	g.Printf("}\n")
-//}
-//
-//// buildMap handles the case where the space is so sparse a map is a reasonable fallback.
-//// It's a rare situation but has simple code.
-//func (g *Generator) buildMap(runs [][]Value, typeName string) {
-//	g.Printf("\n")
-//	g.declareNameVars(runs, typeName, "")
-//	g.Printf("\nvar _%s_map = map[%s]string{\n", typeName, typeName)
-//	n := 0
-//	for _, values := range runs {
-//		for _, value := range values {
-//			g.Printf("\t%s: _%s_name[%d:%d],\n", &value, typeName, n, n+len(value.name))
-//			n += len(value.name)
-//		}
-//	}
-//	g.Printf("}\n\n")
-//	g.Printf(stringMap, typeName)
-//}
-
-// Argument to format is the type name.
-const stringMap = `func (i %[1]s) String() string {
-	if str, ok := _%[1]s_map[i]; ok {
-		return str
-	}
-	return fmt.Sprintf("%[1]s(%%d)", i)
-}
-`
--- testdata/basicstruct.go
+++ testdata/basicstruct.go
@@ -1,9 +1,27 @@
 package main
 
 import "time"
+import "log"
 
-type BasicStruct struct {
+type Basicstruct struct {
 	ID     int
 	Date   time.Time
 	Status string
+}
+
+func main() {
+
+	currentTime := time.Now()
+	bs := Basicstruct{ID: 10, Date: currentTime, Status: "FooBar"}
+
+	if bs.GetID() != 10 {
+		log.Panicf("BasicStruct.GetID() returned: %d not %d", bs.GetID(), 10)
+	}
+	if !currentTime.Equal(bs.GetDate()) {
+		log.Panicf("BasicStruct.GetDate() returned: %v, not %v", bs.GetDate(), currentTime)
+	}
+	if bs.GetStatus() != "FooBar" {
+		log.Panicf("BasicStruct.GetStatus() returned: %s, not %s", bs.GetStatus(), "FooBar")
+	}
+
 }
